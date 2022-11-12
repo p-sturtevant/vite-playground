8cc3b71bd2789f98e51588fd6ef7e48f06d2cfd7
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-12 15:57:17 -0500

    Added MUI and Tailwind css intergration

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
+import { CssBaseline, TextField } from '@mui/material';
+import green from '@mui/material/colors/green';
+import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
-import './App.css';
 import reactLogo from './assets/react.svg';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+		<>
+			<div className="h-fit my-20 mx-auto bg-yellow-50 w-2/3 rounded-xl px-6 pt-6 pb-4 flex justify-start">
+				<TextField
+					variant="standard"
+					fullWidth
+					label="Search for your Favorite Plants"
+					sx={{
+						'& .MuiInput-underline:before': { borderBottomColor: 'stone' },
+						'& .MuiInput-underline:after': {
+							borderBottomColor: 'rgba(4, 120, 87)',
+						},
+						'& .css-aqpgxn-MuiFormLabel-root-MuiInputLabel-root': {
+							color: 'stone',
+						},
+						'& .css-1c2i806-MuiFormLabel-root-MuiInputLabel-root.Mui-focused': {
+							color: 'rgba(4, 120, 87)',
+						},
+					}}
+				></TextField>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
+			<div className="h-fit my-20 mx-auto bg-yellow-50 w-2/3 rounded-xl px-6 pt-6 pb-4 flex justify-start"></div>
+		</>
 	);
 }
 
